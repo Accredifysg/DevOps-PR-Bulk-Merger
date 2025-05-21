ef4386fb7a6892753ce819b18a636c77f427023f
Muhammad Reza Ramadhan <reza.ramadhan@accredify.io>
2025-05-21 17:24:59 +0800

    Adjust script to work with org:Accredifysg

--- bulk_merger.rb
+++ bulk_merger.rb
@@ -48,7 +48,8 @@ class BulkMerger
     puts "Found #{unmerged_pull_requests.size} reviewed but unmerged PRs:\n\n"
 
     unmerged_pull_requests.each do |pr|
-      puts "- '#{pr.title}' (#{pr.html_url}) "
+      repo = pr.repository_url.gsub("https://api.github.com/repos/", "")
+      puts "- #{repo} '#{pr.title}' (#{pr.html_url}) "
     end
 
     puts "\nHave you reviewed the changes, and you want to MERGE all these PRs? [y/N]\n"
@@ -64,9 +65,22 @@ class BulkMerger
 
       repo = pr.repository_url.gsub("https://api.github.com/repos/", "")
 
+      commit_message = <<~MSG
+        #{pr.title}
+
+        Automerged by bulk-merger from #{pr.html_url}
+      MSG
+
       begin
-        client.merge_pull_request(repo, pr.number)
+        client.merge_pull_request(
+          repo, 
+          pr.number, 
+          commit_message,
+          merge_method: "squash",
+        )
         puts "✅"
+
+        sleep 10 # Add delay between merges
       rescue Octokit::MethodNotAllowed => e
         puts "❌ Failed to merge: #{e.message.inspect}"
       end
@@ -74,20 +88,20 @@ class BulkMerger
   end
 
   def self.search_pull_requests(query)
-    client.search_issues("#{query} archived:false is:pr user:alphagov state:open in:title").items
+    client.search_issues("#{query} archived:false is:pr state:open in:title").items
   end
 
   def self.govuk_repos
-    @govuk_repos ||= client.search_repos("org:alphagov topic:govuk")
+    @govuk_repos ||= client.search_repos("org:accredifysg")
       .items
       .reject!(&:archived)
       .map { |repo| repo.full_name }
   end
 
   def self.find_govuk_pull_requests(query)
-    search_pull_requests(query).select do |pr|
-      govuk_repos.any? { |repo| pr.repository_url.include?(repo) }
-    end
+    search_pull_requests(query)
+      .select { |pr| govuk_repos.any? { |repo| pr.repository_url.include?(repo) } }
+      .sort_by { |pr| pr.repository_url.gsub("https://api.github.com/repos/", "") }
   end
 
   def self.client
